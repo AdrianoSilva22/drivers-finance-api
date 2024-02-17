4c834a5bbc5082362e3828151e18894fb63b20b3
AdrianoSilva22 <Adrianodesilva22@gmail.com>
2024-02-17 19:17:12 -0300

    adicionando variáveis de ambiente

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,11 +1,12 @@
-import mysql from 'mysql2/promise'
+import mysql from 'mysql2/promise';
+require('dotenv/config');
 
 const connection = mysql.createPool({
-  host:'localhost',
-  port:3306,
-  user: 'root',
-  password:'1234',
-  database:'drivers_finances'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
 
 export default connection
