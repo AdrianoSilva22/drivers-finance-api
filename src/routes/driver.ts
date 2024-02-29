8cbadf51c3b9b60154618021698b1d6ce0bcf4db
adrianosilva22 <adrianodesilva22@gmail.com>
2024-02-29 20:07:33 -0300

    adicionando a função dateTimeMySql a um Utils

--- src/routes/driver.ts
+++ src/routes/driver.ts
@@ -8,28 +8,14 @@ import { driverRepository } from '../repositories/driverRepository';
 import { handleRequestValidation } from '../utils/expressValidationUtils';
 import { getTotalExpense } from './expense';
 import { getTotalIncome } from './income';
+import { dateTimeMysqlUtils } from '../utils/dateTimeMySqlUtils';
 dotenv.config()
 
 
 const router = express.Router()
 const { login } = driverRepository()
 
-export const getCurrentDateTimeMySQLFormat = () => {
-  const now = new Date()
-
-  const year = now.getFullYear()
-  const month = (now.getMonth() + 1).toString().padStart(2, '0')
-  const day = now.getDate().toString().padStart(2, '0')
-
-  const hours = now.getHours().toString().padStart(2, '0')
-  const minutes = now.getMinutes().toString().padStart(2, '0')
-  const seconds = now.getSeconds().toString().padStart(2, '0')
-
-  const mysqlDatetime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-
-  return mysqlDatetime
-}
-
+const { getCurrentDateTimeMySQLFormat } = dateTimeMysqlUtils()
 const verify =
 
   router.post('/save',
